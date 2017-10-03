e8f20d1b492399582f4e886e676b789410deb862
Yan Sern <hey@yansern.io>
2017-10-03 13:14:35 +0200

    Revert "0.9.4"
    This reverts commit 74b34bd71bac5c13a894e8e623dfec46d7c95ac4.

--- dist/vue-multipane.js
+++ dist/vue-multipane.js
@@ -15,6 +15,10 @@ var __vue_module__ = {
       type: String,
       default: LAYOUT_VERTICAL,
     },
+    classname: {
+      type: String,
+      default: '',
+    },
   },
 
   data: function data() {
@@ -28,7 +32,8 @@ var __vue_module__ = {
       return [
         'multipane',
         'layout-' + this.layout.slice(0, 1),
-        this.isResizing ? 'is-resizing' : '' ];
+        this.isResizing ? 'is-resizing' : '',
+        this.classname ];
     },
     cursor: function cursor() {
       return this.isResizing
@@ -40,96 +45,93 @@ var __vue_module__ = {
     },
   },
 
-  events: {
-    paneResizeBeforeStart: function paneResizeBeforeStart(pane, resizer, initialPageX, initialPageY) {
-      var self = this;
-      var container = self.$el;
-      var layout = self.layout;
+  methods: {
+    onMouseDown: function onMouseDown(ref) {
+      var resizer = ref.target;
+      var initialPageX = ref.pageX;
+      var initialPageY = ref.pageY;
 
-      var initialPaneWidth = pane.offsetWidth;
-      var initialPaneHeight = pane.offsetHeight;
+      if (resizer.className.match('multipane-resizer')) {
+        var self = this;
+        var container = self.$el;
+        var layout = self.layout;
 
-      var usePercentage = !!(pane.style.width + '').match('%');
+        var pane = resizer.previousElementSibling;
+        var initialPaneWidth = pane.offsetWidth;
+        var initialPaneHeight = pane.offsetHeight;
 
-      var addEventListener = window.addEventListener;
-      var removeEventListener = window.removeEventListener;
+        var usePercentage = !!(pane.style.width + '').match('%');
 
-      var resize = function (initialSize, offset) {
-        if ( offset === void 0 ) offset = 0;
+        var addEventListener = window.addEventListener;
+        var removeEventListener = window.removeEventListener;
 
-        if (layout == LAYOUT_VERTICAL) {
-          var containerWidth = container.clientWidth;
-          var paneWidth = initialSize + offset;
+        var resize = function (initialSize, offset) {
+          if ( offset === void 0 ) offset = 0;
 
-          return (pane.style.width = usePercentage
-            ? paneWidth / containerWidth * 100 + '%'
-            : paneWidth + 'px');
-        }
+          if (layout == LAYOUT_VERTICAL) {
+            var containerWidth = container.clientWidth;
+            var paneWidth = initialSize + offset;
 
-        if (layout == LAYOUT_HORIZONTAL) {
-          var containerHeight = container.clientHeight;
-          var paneHeight = initialSize + offset;
+            return (pane.style.width = usePercentage
+              ? paneWidth / containerWidth * 100 + '%'
+              : paneWidth + 'px');
+          }
 
-          return (pane.style.height = usePercentage
-            ? paneHeight / containerHeight * 100 + '%'
-            : paneHeight + 'px');
-        }
-      };
+          if (layout == LAYOUT_HORIZONTAL) {
+            var containerHeight = container.clientHeight;
+            var paneHeight = initialSize + offset;
 
-      var onMouseMove = function(ref) {
-        var pageX = ref.pageX;
-        var pageY = ref.pageY;
+            return (pane.style.height = usePercentage
+              ? paneHeight / containerHeight * 100 + '%'
+              : paneHeight + 'px');
+          }
+        };
 
-        var size =
-          layout == LAYOUT_VERTICAL
-            ? resize(initialPaneWidth, pageX - initialPageX)
-            : resize(initialPaneHeight, pageY - initialPageY);
+        // This adds is-resizing class to container
+        self.isResizing = true;
 
-        self.$emit('paneResize', pane, resizer, size, pageX, pageY);
-      };
+        // Resize once to get current computed size
+        var size = resize();
 
-      var onMouseUp = function(ref) {
-        var pageX = ref.pageX;
-        var pageY = ref.pageY;
+        // Trigger paneResizeStart event
+        self.$emit('paneResizeStart', pane, resizer, size);
 
-        // Run resize one more time to set computed width/height.
-        var size =
-          layout == LAYOUT_VERTICAL
-            ? resize(pane.clientWidth)
-            : resize(pane.clientHeight);
+        var onMouseMove = function(ref) {
+          var pageX = ref.pageX;
+          var pageY = ref.pageY;
 
-        // This removes is-resizing class to container
-        self.isResizing = false;
+          size =
+            layout == LAYOUT_VERTICAL
+              ? resize(initialPaneWidth, pageX - initialPageX)
+              : resize(initialPaneHeight, pageY - initialPageY);
 
-        removeEventListener('mousemove', onMouseMove);
-        removeEventListener('mouseup', onMouseUp);
+          self.$emit('paneResize', pane, resizer, size);
+        };
 
-        self.$emit('paneResizeStop', pane, resizer, size, pageX, pageY);
-      };
+        var onMouseUp = function() {
+          // Run resize one more time to set computed width/height.
+          size =
+            layout == LAYOUT_VERTICAL
+              ? resize(pane.clientWidth)
+              : resize(pane.clientHeight);
 
-      addEventListener('mousemove', onMouseMove);
-      addEventListener('mouseup', onMouseUp);
+          // This removes is-resizing class to container
+          self.isResizing = false;
 
-      // This adds is-resizing class to container
-      self.isResizing = true;
+          removeEventListener('mousemove', onMouseMove);
+          removeEventListener('mouseup', onMouseUp);
 
-      // Resize once to get current computed size
-      var size = resize();
+          self.$emit('paneResizeStop', pane, resizer, size);
+        };
 
-      // Trigger paneResizeStart event
-      self.$emit(
-        'paneResizeStart',
-        pane,
-        resizer,
-        size,
-        initialPageX,
-        initialPageY
-      );
+        addEventListener('mousemove', onMouseMove);
+        addEventListener('mouseup', onMouseUp);
+      }
     },
   },
 };
 
-(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=".multipane { display: flex; } .multipane.layout-h { flex-direction: column; } .multipane.layout-v { flex-direction: row; } .multipane > div { position: relative; z-index: 1; } .multipane-resizer { display: block; position: relative; z-index: 2; } .layout-h > .multipane-resizer { width: 100%; height: 10px; margin-top: -10px; top: 5px; cursor: row-resize; } .layout-v > .multipane-resizer { width: 10px; height: 100%; margin-left: -10px; left: 5px; cursor: col-resize; } "; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
+(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=".multipane { display: flex; } .multipane.layout-h { flex-direction: column; } .multipane.layout-v { flex-direction: row; } "; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
 
 
 
@@ -150,49 +152,41 @@ var __vue_module__ = {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    var __$__vue_module__ = Object.assign(__vue_module__, {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{class:_vm.classnames,style:({ cursor: _vm.cursor, userSelect: _vm.userSelect })},[_vm._t("default")],2)},staticRenderFns: [],});
+    var __$__vue_module__ = Object.assign(__vue_module__, {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{class:_vm.classnames,style:({ cursor: _vm.cursor, userSelect: _vm.userSelect }),on:{"mousedown":_vm.onMouseDown}},[_vm._t("default")],2)},staticRenderFns: [],});
     __$__vue_module__.prototype = __vue_module__.prototype;
 
-(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=""; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
+(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=".multipane > div { position: relative; z-index: 1; } .multipane-resizer { display: block; position: relative; z-index: 2; } .layout-h > .multipane-resizer { width: 100%; height: 10px; margin-top: -10px; top: 5px; cursor: row-resize; } .layout-v > .multipane-resizer { width: 10px; height: 100%; margin-left: -10px; left: 5px; cursor: col-resize; } "; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
 
 
 
-var MultipaneResizer = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"multipane-resizer",on:{"mousedown":_vm.onMouseDown}},[_vm._t("default")],2)},staticRenderFns: [],
-    name: 'multipane-resizer',
-    methods: {
-        onMouseDown: function onMouseDown(ref) {
-            var resizer = this.$el;
-            var pane = resizer.previousElementSibling;
-            this.$emit('paneResizeBeforeStart', pane, resizer, pageX, pageY);
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var MultipaneResizer = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"multipane-resizer"},[_vm._t("default")],2)},staticRenderFns: [],
 };
 
 if (typeof window !== 'undefined' && window.Vue) {
