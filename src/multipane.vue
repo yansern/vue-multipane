68c559b9340c051a55871bdc1d33c3b5c17b3d68
drrainlab <renkornator@gmail.com>
2019-01-25 13:51:07 +0300

    Update multipane.vue
    Prevent "mousedown" event from bubbling up which involves in nested multipanes conflict.

--- src/multipane.vue
+++ src/multipane.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="classnames" :style="{ cursor, userSelect }" @mousedown="onMouseDown">
+  <div :class="classnames" :style="{ cursor, userSelect }" @mousedown.stop="onMouseDown">
     <slot></slot>
   </div>
 </template>
